d2de693f503140fa6d14344cd1b9f66efe5decbe
nveshaan <nveshaan@gmail.com>
2026-03-30 09:35:59 +0530

    add year-wise db

--- db.py
+++ db.py
@@ -1,22 +1,23 @@
 import sqlite3
 import os
 
-def get_db():
-    conn = sqlite3.connect("f1.db")
+def get_db(path: str="f1.db"):
+    conn = sqlite3.connect(path)
     conn.row_factory = sqlite3.Row
     return conn
 
-def init_db(reset=False):
-    if os.path.exists("f1.db") and not reset:
+def init_db(year, reset=False):
+    path = str(year) + ".db"
+    if os.path.exists(path=path) and not reset:
         print("Database already exists. Use reset=True to overwrite.")
         return
 
     with open('schema.sql') as f:
-        if os.path.exists("f1.db"):
-            os.remove("f1.db")
-        db = get_db()
+        if os.path.exists(path=path):
+            os.remove(path)
+        db = get_db(path)
         db.executescript(f.read())
         db.commit()
 
 if __name__ == '__main__':
-    init_db()
+    init_db(year=2017, reset=True)
